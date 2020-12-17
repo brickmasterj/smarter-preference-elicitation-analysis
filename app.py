68de867a18d696be51f4b4682aeb86446e14a677
Joris Hoeksema <jorishoek@gmail.com>
2020-12-17 20:58:58 +0000

    Implementated LighthouseStudio data format

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # Main app script
 import dash
+import json
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objects as go
+import pandas as pd
 from data import format_data
 
 
@@ -14,7 +16,17 @@ def graph_element(key, weight, fig):
         html.H2(children='{} {}'.format(key, weight)),
         dcc.Graph(figure=fig)])
 
-attributes = format_data()
+# Try if the default files are located in the root dir, otherwise initiate with example data
+try:
+    csv_data = pd.read_csv('input_data.csv')
+    with open('input_data_labels.json') as input_data_labels_file:
+        input_data_labels = json.load(input_data_labels_file)
+        csv_labels = input_data_labels['csv_labels']
+        csv_type = input_data_labels['csv_type']
+
+    attributes = format_data(csv_data, csv_labels, csv_type)
+except:
+   attributes = format_data()
 
 # Generate Dash graphs from figures, with title in a <div>
 graphs = [graph_element(key, attribute['weight'], attribute['fig']) for key, attribute in attributes.items()]
--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 # Convert input data into usable attributes dictionary
 import plotly.graph_objects as go
+import pandas as pd
 
 def generate_graphs(attributes):
     ''' Generate figure for each attribute in
@@ -30,14 +31,41 @@ def generate_graphs(attributes):
     return attributes
 
 
-def format_data(csv_data = None, csv_type = None):
+def format_data_lighthousestudio(csv_data, csv_labels, weighing_method = 'ROC'):
+    ''' Format data from Lighthouse Studio from Sawtooth Software
+    '''
+    for attribute_name, attribute in csv_labels.items():
+        # Generate level data
+        # Initiate empty lists
+        attribute['values'] = []
+        attribute['pos_error'] = []
+        attribute['neg_error'] = []
+
+        # Loop over each level and calculate mean and std
+        for level in attribute['level_col_names']:
+            level_data = csv_data[level]
+            level_mean = level_data.mean()
+            level_std = level_data.std()
+
+            attribute['values'].append(level_mean)
+            attribute['pos_error'].append(level_std)
+            attribute['neg_error'].append(level_std)
+
+        # Generate weights
+        attribute['weight'] = 1 # TODO: REMOVE DUMMY WEIGHTs
+
+    return csv_labels
+
+
+def format_data(csv_data = None, csv_labels = None, csv_type = None, weighing_method = 'ROC'):
     ''' Formated data into usable attributes dict,
     given input csv data, as well as specifying the type.
+    Also given a certain weighing method.
     If no csv is provided, return example data.
     '''
-    if csv_data is not None:
-        # TODO: Implement formatting data
-        return dict()
+    if csv_type == 'LighthouseStudio':
+        attributes_dataset = format_data_lighthousestudio(csv_data, csv_labels, weighing_method)
+        return generate_graphs(attributes_dataset)
     else:
         # Return example data
         # Example attributes dataset
@@ -46,10 +74,18 @@ def format_data(csv_data = None, csv_type = None):
                 'type': 'numerical',                        # Type of the attribute
                 'weight': 0.75,                             # Weight distributed to attribute based on selected weighin method
                 'levels': [10, 20, 30, 40, 50],             # Levels of the attribute, array[int||float] for numerical, array[str] for nominal
+                'level_col_names': ['Cost_r1',              # Names of the columns for each level in the dataset
+                    'Cost_r2', 
+                    'Cost_r3', 
+                    'Cost_r4', 
+                    'Cost_r5'], 
+                'min_value_level': 1,                       # The minimum value that can be inputted by a user for a level
+                'max_value_level': 7,                       # The maximum value that can be inputted by a user for a level
                 'values': [0.9, 0.5, 0.2, 0.15, 0.1],       # Average value elicited for each level
                 'pos_error': [0.1, 0.2, 0.1, 0.05, 0.02],   # Error bar in positive direction for each level
                 'neg_error': [0.2, 0.1, 0.05, 0.1, 0.1],    # Error bar in negative direction for each level
-                'x_label': 'Cost [€]'},                     # Label for x-axis
+                'x_label': 'Cost [€]',                      # Label for x-axis
+                'ranking_id': 1},                           # ID given to this attribute in the ranking question
             'Happiness': {                                  # Name of the attribute, must be unique!
                 'type': 'nominal',                          # Type of the attribute
                 'weight': 0.25,                             # Weight distributed to attribute based on selected weighin method
@@ -57,9 +93,16 @@ def format_data(csv_data = None, csv_type = None):
                     'A bit',
                     'Average amount',
                     'A lot'],             
+                'level_col_names': ['Happiness_r1',         # Names of the columns for each level in the dataset
+                    'Happiness_r2', 
+                    'Happiness_r3', 
+                    'Happiness_r4'],                             
+                'min_value_level': 1,                       # The minimum value that can be inputted by a user for a level
+                'max_value_level': 7,                       # The maximum value that can be inputted by a user for a level
                 'values': [0.2, 0.4, 0.6, 0.8],             # Average value elicited for each level
                 'pos_error': [0.1, 0.2, 0.1, 0.2],          # Error bar in positive direction for each level
                 'neg_error': [0.2, 0.3, 0.2, 0.1],          # Error bar in negative direction for each level
-                'x_label': 'Happiness [-]'}}                # Label for x-axis
+                'x_label': 'Happiness [-]',                 # Label for x-axis
+                'ranking_id': 1}}                           # ID given to this attribute in the ranking question
 
         return generate_graphs(example_attributes_dataset)
